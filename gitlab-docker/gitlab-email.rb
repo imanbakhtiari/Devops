c63af44c9adc39efdc86720e62b380a62179505a
imanbakhtiari <imanbakhtiarinet@basa.ir>
2024-05-25 13:08:48 +0330

    add gitlab

---
+++ gitlab-docker/gitlab-email.rb
@@ -0,0 +1,14 @@
+gitlab_rails['smtp_enable'] = true
+gitlab_rails['smtp_address'] = "smtp.gmail.com"
+gitlab_rails['smtp_port'] = 587
+#gitlab_rails['smtp_user_name'] = "gitbasa@gmail.com"
+gitlab_rails['smtp_user_name'] = "youremail@gmail.com"
+gitlab_rails['smtp_password'] = "zgki abvp jryv ucxa"
+gitlab_rails['smtp_domain'] = "smtp.gmail.com"
+gitlab_rails['smtp_authentication'] = "login"
+gitlab_rails['smtp_enable_starttls_auto'] = true
+gitlab_rails['smtp_tls'] = false
+gitlab_rails['smtp_openssl_verify_mode'] = 'peer'
+gitlab_rails['gitlab_email_from'] = 'youremail@gmail.com'
+gitlab_rails['gitlab_email_reply_to'] = 'youremail@gmail.com'
+
