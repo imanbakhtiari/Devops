b33c579b954428b782da404a23da8b5e053b06a3
imanbakhtiari <imanbakhtiarinet@basa.ir>
2024-06-16 16:24:06 +0330

    commands

---
+++ commands/show-maximumusage.sh
@@ -0,0 +1,6 @@
+## show maximum usage of all process
+```bash
+ps -eo size,pid,user,command --sort -size | \
+    awk '{ hr=$1/1024 ; printf("%13.2f Mb ",hr) } { for ( x=4 ; x<=NF ; x++ ) { printf("%s ",$x) } print "" }' |\
+    cut -d "" -f2 | cut -d "-" -f1
+```
