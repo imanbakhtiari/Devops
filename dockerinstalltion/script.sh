b4b6dd0271b47fe6831a4cebd5d734457adc9f43
imanbakhtiari <imanbakhtiarinet@basa.ir>
2024-08-31 10:15:02 +0330

    dockerinstall

--- dockerinstalltion/script.sh
+++ dockerinstalltion/script.sh
@@ -13,4 +13,4 @@ echo \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
