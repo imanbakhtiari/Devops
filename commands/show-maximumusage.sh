5d4af866d1727a7bf39cb7673cbb692d03f7ec36
imanbakhtiari <imanbakhtiarinet@basa.ir>
2024-09-23 16:36:00 +0330

    add maximumusagecpu

--- commands/show-maximumusage.sh
+++ commands/show-maximumusage.sh
@@ -1,3 +1,9 @@
 ps -eo size,pid,user,command --sort -size | \
     awk '{ hr=$1/1024 ; printf("%13.2f Mb ",hr) } { for ( x=4 ; x<=NF ; x++ ) { printf("%s ",$x) } print "" }' |\
     cut -d "" -f2 | cut -d "-" -f1
+
+## also for cpua nd aram both
+ps -eo size,%cpu,pid,user,command --sort -size | \
+awk '{ mem=$1/1024 ; cpu=$2 ; printf("%13.2f Mb %6.2f%% CPU ", mem, cpu) } { for ( x=5 ; x<=NF ; x++ ) { printf("%s ",$x) } print "" }' |\
+cut -d "" -f2 | cut -d "-" -f1
+
